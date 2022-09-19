fe3e4b5c6287f32d16b96741772ac7b2dcdce83c
goncalveshigino <goncalveshigino20@gmail.com>
2022-09-19 22:00:27 +0100

    hg

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -3,10 +3,12 @@ import 'package:quitanda/src/auth/components/custom_text_field.dart';
 
 
 class SignInScreen extends StatelessWidget {
+  
   const SignInScreen({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
+
     return Scaffold(
        backgroundColor: Colors.green,
        body:Column(
@@ -20,7 +22,7 @@ class SignInScreen extends StatelessWidget {
             child: Container(
               padding: const EdgeInsets.symmetric(
                 horizontal: 32,
-                vertical: 40
+                vertical: 27
               ),
              decoration: const BoxDecoration(
                 color: Colors.white,
@@ -29,16 +31,48 @@ class SignInScreen extends StatelessWidget {
                  )
              ),
              child: Column(
-              children:  [
-                CustomTextField(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children:   [
+               const CustomTextField(
                   icon: Icons.email,
                   label: 'Email',
                  
                 ),
-                CustomTextField(
+               const CustomTextField(
                   icon: Icons.lock,
                   label: 'Senha',
                   isSecret: true,
+                ),
+
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)
+                      )
+                    ),
+                    onPressed:(){}, 
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    )
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: (){}, 
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(
+                        color: Colors.red
+                      ),
+                    )
+                  ),
                 )
               
               ],
