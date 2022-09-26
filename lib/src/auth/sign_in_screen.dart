169b29fba4d5dbeedf57ccaeaf7911ae7e564e24
goncalveshigino <goncalveshigino20@gmail.com>
2022-09-26 22:33:51 +0100

    page view adicionado

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanda/src/auth/components/custom_text_field.dart';
 import 'package:quitanda/src/auth/sign_up_screen.dart';
+import 'package:quitanda/src/base/base_screen.dart';
 import 'package:quitanda/src/config/custom_colors.dart';
 
 
@@ -109,7 +110,15 @@ class SignInScreen extends StatelessWidget {
                           borderRadius: BorderRadius.circular(18)
                         )
                       ),
-                      onPressed:(){}, 
+                      onPressed:(){
+
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (c){
+                            return  const BaseScreen();
+                          },),
+                        );
+
+                      }, 
                       child: const Text(
                         'Entrar',
                         style: TextStyle(
---
+++ lib/src/base/base_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.brown,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(index);
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Carrinho',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
+  }
+}
